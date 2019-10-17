934e5419295905ae4900e3892c3fdfee9ed94a92
alex scalese <mail@alex-scalese.de>
2019-10-18 00:16:32 +0200

    Add basic test class

---
+++ workspace/college-management-system/src/test/java/de/scalese/springboot/collegemanagementsystem/CollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.scalese.springboot.collegemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

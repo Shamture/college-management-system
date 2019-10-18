835803829a6ec6af5df969a700bcab2232477986
alex scalese <mail@alex-scalese.de>
2019-10-18 22:24:05 +0200

    fix this class

--- workspace/college-management-system/src/main/java/de/scalese/springboot/collegemanagementsystem/entity/Student.java
+++ workspace/college-management-system/src/main/java/de/scalese/springboot/collegemanagementsystem/entity/Student.java
@@ -12,15 +12,15 @@ public class Student {
     private int id;
 
     @Column(name = "first_name")
-    private int firstName;
+    private String firstName;
 
     @Column(name = "last_name")
-    private int lastName;
+    private String lastName;
 
     @Column(name = "email")
-    private int email;
+    private String email;
 
-    public Student(int firstName, int lastName, int email) {
+    public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -36,31 +36,31 @@ public class Student {
         this.id = id;
     }
 
-    public int getFirstName() {
-        return firstName;
-    }
+    public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(int firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public int getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(int lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public int getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(int email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    @Override
+	@Override
     public String toString() {
         return "Student{" +
                 "id=" + id +

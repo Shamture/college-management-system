1f5b43f8199fbd9839bd9639a5080685e0c28677
alex scalese <mail@alex-scalese.de>
2019-10-20 11:21:58 +0200

    Add course entity

---
+++ workspace/college-management-system/src/main/java/de/scalese/springboot/collegemanagementsystem/entity/Course.java
@@ -0,0 +1,70 @@
+package de.scalese.springboot.collegemanagementsystem.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "title")
+	private String title;
+	
+	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, 
+						  CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name = "instructor_id")
+	@JsonBackReference
+	private Instructor instructor;
+
+	public Course(String title, Instructor instructor) {
+		this.title = title;
+		this.instructor = instructor;
+	}
+
+	public Course() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+	
+}
